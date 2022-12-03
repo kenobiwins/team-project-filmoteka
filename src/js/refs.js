2af2b534eeb7dad3dc2c605f92bc5d26d414fddf
Max Sidorenko <fivemoshr@gmail.com>
2022-12-03 21:01:54 +0200

    update form

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,4 +3,5 @@ export { refs };
 const refs = {
   btnWatched: document.querySelector('#btn-watched'),
   btnQueue: document.querySelector('#btn-queue'),
+  form: document.querySelector('.page-header__form'),
 };
