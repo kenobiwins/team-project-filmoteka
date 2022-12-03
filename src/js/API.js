c2f72ef3eed9f09db4e7defcfbeaf7f0dc0cf6ba
fedorzvyk <fedorzvyk@gmail.com>
2022-12-03 15:37:07 +0200

    edit API

--- src/js/API.js
+++ src/js/API.js
@@ -5,41 +5,57 @@ export { getPopularMovies, searchMovies, getMovieById, getGenresList };
 const API_KEY = 'a6c77db842e34b0d748568175c4ac730';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 
-async function getPopularMovies(page) {
-  const response = await axios.get(
-    `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${page}`
-  );
-  if (!response.status) {
-    throw new Error(response.status);
+async function getPopularMovies(page = 1) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${page}`
+    );
+    if (!response.status) {
+      throw new Error('No trending films anymore');
+    }
+    return response;
+  } catch (error) {
+    console.log(error);
   }
-  return response.data.results;
 }
 
-async function searchMovies(searchQuery, page) {
-  const response = await axios.get(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${page}`
-  );
-  if (!response.status) {
-    throw new Error(response.status);
+async function searchMovies(searchQuery, page = 1) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${page}`
+    );
+    if (!response.status) {
+      throw new Error('No results found for your search');
+    }
+    return response;
+  } catch (error) {
+    console.log(error);
   }
-  return response.data.results;
 }
 
 async function getMovieById(id) {
-  const response = await axios.get(
-    `${BASE_URL}/movie/${id}?api_key=${API_KEY}`
-  );
-  if (!response.status) {
-    throw new Error(response.status);
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}`
+    );
+    if (!response.status) {
+      throw new Error('This movie is not available');
+    }
+    return response;
+  } catch (error) {
+    console.log(error);
   }
-  return response.data;
 }
 
 async function getGenresList() {
-  const URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
-  const response = await axios.get(`${URL}`);
-  if (!response.status) {
-    throw new Error(response.status);
+  try {
+    const URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+    const response = await axios.get(`${URL}`);
+    if (!response.status) {
+      throw new Error('Genres list is not available');
+    }
+    return response.data.genres;
+  } catch (error) {
+    console.log(error);
   }
-  return response.data.genres;
 }
