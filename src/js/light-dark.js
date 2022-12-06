fc170650783b8c41a2394dcdd9189757b4cb9c69
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-12-06 14:53:42 +0200

    add new

--- src/js/light-dark.js
+++ src/js/light-dark.js
@@ -1,16 +1,15 @@
 const toggleSwitch = document.querySelector('.theme');
 const lightIcon = document.querySelector('.lightTheme__icon');
 const darkIcon = document.querySelector('.darkTheme__icon');
+const html = document.querySelector('html');
+const footerChange = document.querySelector('.footer');
 toggleSwitch.addEventListener('click', switchTheme);
-
-function switchTheme(e) {
-  console.log(e.target);
-  console.log('e.target');
+function switchTheme() {
   //   document.documentElement.classList.toggle('dark');
-  document.body.classList.toggle('dark');
+  html.classList.toggle('dark');
   lightIcon.classList.toggle('is-darkTheme');
   darkIcon.classList.toggle('is-lightTheme');
-
+  footerChange.classList.toggle('footer-color');
   //   if (e.target) {
   // document.html.classList.toggle('dark');
 
