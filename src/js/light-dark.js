31abeb2321ec896cd341e805787538192b32c0f8
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-12-06 19:23:00 +0200

    add LS

--- src/js/light-dark.js
+++ src/js/light-dark.js
@@ -4,11 +4,30 @@ const darkIcon = document.querySelector('.darkTheme__icon');
 const html = document.querySelector('html');
 const footerChange = document.querySelector('.footer');
 
-toggleSwitch.addEventListener('click', switchTheme);
+const darkLibrary = document.querySelector('.site-nav__link');
+toggleSwitch.addEventListener('click', setTheme);
+let savedTheme = '';
+window.addEventListener('load', rememberDarkForLib);
+// darkLibrary.addEventListener('click', rememberDarkForLib);
 
+function setTheme() {
+  switchTheme();
+  localStorage.setItem('ui-theme', html.classList.value);
+}
 function switchTheme() {
   html.classList.toggle('dark');
   lightIcon.classList.toggle('is-darkTheme');
   darkIcon.classList.toggle('is-lightTheme');
   footerChange.classList.toggle('footer-color');
+}
+
+function rememberDarkForLib(e) {
+  savedTheme = localStorage.getItem('ui-theme');
+  console.log(savedTheme);
+
+  if (savedTheme !== 'dark') {
+    return;
+  } else {
+    switchTheme();
+  }
 }
